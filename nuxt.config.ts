05aa26a4259ee8c2349f776cc1ffe7203dd0b017
Tomislav Dimitrov <nastallanbg@gmail.com>
2024-07-11 10:15:41 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'], 
+  css: ['~/assets/css/main.css'],
+
+  head: {
+    script: [],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
+        defer: true
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Unbounded:wght@200..900&display=swap',
+        defer: true
+      }
+    ]
+  },
+
 })
